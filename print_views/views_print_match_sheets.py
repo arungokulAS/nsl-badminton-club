692d67cb7532f15ae990c3d3af4379520ae762a8
Arun Gokul <arungokul@Aruns-MacBook-Pro.local>
2026-03-19 10:55:39 +0000

    Redesign score sheet print layout

--- print_views/views_print_match_sheets.py
+++ print_views/views_print_match_sheets.py
@@ -13,7 +13,13 @@ STANDARD_ROUND_NAMES = [
 
 def print_match_sheets(request):
     matches = Match.objects.select_related('team1', 'team2', 'court', 'round').filter(round__order__in=[1, 2, 3, 4, 5, 6, 7], round__name__in=STANDARD_ROUND_NAMES).order_by('round__order', 'court__id')
+    sheets = [
+        {'points': 15, 'rows': range(1, 16)},
+        {'points': 21, 'rows': range(1, 22)},
+        {'points': 30, 'rows': range(1, 31)},
+    ]
     context = {
         'matches': matches,
+        'sheets': sheets,
     }
     return render(request, 'print_views/print_match_sheets.html', context)
