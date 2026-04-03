4223a6257146151c589b1e2ccf984f4b2346c37e
Arun Gokul <arungokul@Aruns-MacBook-Pro.local>
2026-04-03 19:47:28 +0100

    Resolve results migration branch conflict

---
+++ results/migrations/0003_merge_0002_score_set_fields_0002_score_set_submitted_fields.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('results', '0002_score_set_fields'),
+        ('results', '0002_score_set_submitted_fields'),
+    ]
+
+    operations = []
